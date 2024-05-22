dcc815f2f455993b6a660f1cd23e4730441395de
Azamat Gaparov <agaparovkg@gmail.com>
2024-05-22 16:25:59 -0700

    new changes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,23 @@
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="login-page">
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div className="credentials email">
+          <label>Email:</label>
+          <input type="email" value={email} required />
+        </div>
+        <div className="credentials">
+          <label>Password:</label>
+          <input type="password" value={password} required />
+        </div>
+        <button className="login-btn" type="submit">
+          Login
+        </button>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+      </form>
+    </div>
+  );
 }
 
 export default Login;
